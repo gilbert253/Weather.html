8d2d3d130d9cb7104b85ebacef6d1c08bf0c2efd
gilbert253 <167355373+gilbert253@users.noreply.github.com>
2024-06-03 19:57:07 -0700

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,1 +1,27 @@
+const input = document.getElementById("city-input");
+const btn = document.getElementById("submit-button");
+const temperature = document.getElementById("temperature");
+const wind = document.getElementById("wind");
+const clouds = document.getElementById("cloud");
+
+
+
+let weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=17b21eed487c55d2ec2bac78727c83dc`;
+
+const getFetch = () =>{
+  fetch(weatherApi).
+  then(res => res.json()).
+  then(data=>{
+    temperature.textInput = `${data.temperature.value}`;
+    wind.textInput = `${data.wind.speed}`;
+    clouds.textInput = `${data.clouds.value}`;
+  })
+  .catch(error => {
+    console.log(error)//is there more that should go into the errors?
+  })
+};
+
+btn.addEventListener("click", getFetch);
+
+//api key 17b21eed487c55d2ec2bac78727c83dc
 
